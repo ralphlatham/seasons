238d97e583cc0e3e230b8ce5edfee2c39da4ebd5
ralphlatham <ralph.l.latham@gmail.com>
2022-05-22 00:16:40 +0100

    Added seasons React app
    Added files for seasons React app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const App = () => {
+//    window.navigator.geolocation.getCurrentPosition(
+//        (position) => console.log(position),
+//        (err) => console.log(err)
+//    );
+
+//    return <div>Latitude: </div>;
+//};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // We called setState!!!
+                this.setState({ lat: position.coords.latitude })
+
+                // DO NOT DO THIS!! 
+                // this.state.lat = position.coords.latitude
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    };
+
+    // React says we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
+    };
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
