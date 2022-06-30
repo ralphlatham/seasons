0548e677e994162b155abb1bb612506db0c55d70
ralphlatham <ralph.l.latham@gmail.com>
2022-06-30 12:13:09 +0100

    Ongoing work on seasons project
    Added files for ongoing work on seasons project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 //const App = () => {
 //    window.navigator.geolocation.getCurrentPosition(
@@ -47,10 +49,11 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            //return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner />;
     };
 };
 
